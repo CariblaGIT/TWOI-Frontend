57741224eff39893155eee71dcd3254ef91e96be
Caribla <caribla4@gmail.com>
2024-05-12 12:44:03 +0200

    Feat: Router Dom implementation with basic views

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import './App.css'
+import { Footer } from './common/Footer/Footer'
+import { Header } from './common/Header/Header'
+import { Body } from './pages/Body/Body'
 
 function App() {
 
   return (
     <>
-      App.js
+      <Header />
+      <Body />
+      <Footer />
     </>
   )
 }
