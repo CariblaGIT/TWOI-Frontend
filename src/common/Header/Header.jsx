9fa49c8a9cf43acfbb33cd55d613b99c01eb3bc6
Caribla <caribla4@gmail.com>
2024-05-13 11:27:12 +0200

    Feat: Header design with logo and dropdown implemented without navigates

--- src/common/Header/Header.jsx
+++ src/common/Header/Header.jsx
@@ -1,9 +1,12 @@
+import { HeaderSection } from "../HeaderSection/HeaderSection";
 import "./Header.css";
 
 export const Header = () => {
     return (
         <div className="headerDesign">
-            
+            <img className="logoImgHeader" src="../../../public/logo.png"/>
+            <h3>The Wiki of Isaac</h3>
+            <HeaderSection/>
         </div>
     )
 }
