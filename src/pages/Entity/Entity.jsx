547ba928e425465bc25a0180e35f578869e9c7a2
Caribla <caribla4@gmail.com>
2024-05-18 21:37:38 +0200

    Feat: Generating table for each entity and changing content

--- src/pages/Entity/Entity.jsx
+++ src/pages/Entity/Entity.jsx
@@ -2,11 +2,15 @@ import "./Entity.css";
 import { useState, useEffect } from "react";
 import { useParams } from 'react-router-dom';
 import { getAllAchievementsService, getAllCharactersService, getAllItemsService, getAllPickupsService } from "../../services/apiCalls";
+import { mainTextAchievements, mainTextCharacters, mainTextItems, mainTextPickups } from '../../utils/entitiesTextConstants';
+import { achievementsProperties, charactersProperties, itemsProperties, pickupsProperties } from '../../utils/entitiesTextConstants';
 
 export const Entity = () => {
     let { type } = useParams()
     const [entityData, setEntityData] = useState([])
-    const [loadedEntityData, setLoadedEntityData] = useState(false)
+    const [entityHeaderData, setEntityHeaderData] = useState([])
+    const [entityText, setEntityText] = useState("")
+    const publicServer = "https://twoi-backend-production.up.railway.app/api/public/"
 
     useEffect(() => {
         const getEntityData = async () => {
@@ -14,23 +18,27 @@ export const Entity = () => {
                 switch (type) {
                     case "achievements":
                         const fetchedAchievements = await getAllAchievementsService()
-                        setLoadedEntityData(true)
                         setEntityData(fetchedAchievements.data)
+                        setEntityHeaderData(achievementsProperties)
+                        setEntityText(mainTextAchievements)
                         break;
                     case "items":
                         const fetchedItems = await getAllItemsService()
-                        setLoadedEntityData(true)
                         setEntityData(fetchedItems.data)
+                        setEntityHeaderData(itemsProperties)
+                        setEntityText(mainTextItems)
                         break;
                     case "characters":
                         const fetchedCharacters = await getAllCharactersService()
-                        setLoadedEntityData(true)
                         setEntityData(fetchedCharacters.data)
+                        setEntityHeaderData(charactersProperties)
+                        setEntityText(mainTextCharacters)
                         break;
                     case "pickups":
                         const fetchedPickups = await getAllPickupsService()
-                        setLoadedEntityData(true)
                         setEntityData(fetchedPickups.data)
+                        setEntityHeaderData(pickupsProperties)
+                        setEntityText(mainTextPickups)
                         break;
                 }
             } catch (error) {
@@ -38,12 +46,79 @@ export const Entity = () => {
             }
         }
     
-        if (!loadedEntityData) { getEntityData() }
+        getEntityData();
+    }, [type])
+
+    useEffect(() => {
+        console.log(entityData)
     }, [entityData])
+
+    const fulfillTable = () => {
+        switch (type) {
+            case "pickups":
+                return entityData.map((item) => (
+                    <tr key={item._id}>
+                        <td>{item.name}</td>
+                        <td>
+                            <img src={publicServer + type + "/" + item.image} alt={item.name} />
+                        </td>
+                        <td className="centerTableContent">{item.description}</td>
+                        <td>{(item.type).toUpperCase()}</td>
+                    </tr>
+                ));
+                case "achievements":
+                    return entityData.map((item) => (
+                        <tr key={item._id}>
+                            <td>{item.name}</td>
+                            <td>
+                                <img src={publicServer + type + "/" + item.image} alt={item.name} />
+                            </td>
+                            <td className="centerTableContent">{item.description}</td>
+                            <td className="centerTableContent">{item.how_to}</td>
+                        </tr>
+                    ));
+                case "items":
+                    return entityData.map((item) => (
+                        <tr key={item._id}>
+                            <td>{item.name}</td>
+                            <td>
+                                <img className="itemsImgs" src={publicServer + type + "/" + item.image} alt={item.name} />
+                            </td>
+                            <td className="centerTableContent">{item.description}</td>
+                            <td>{(item.type).toUpperCase()}</td>
+                        </tr>
+                    ));
+                case "characters":
+                    return entityData.map((item) => (
+                        <tr key={item._id}>
+                            <td>{item.name}</td>
+                            <td>
+                                <img src={publicServer + type + "/" + item.image} alt={item.name} />
+                            </td>
+                            <td className="centerTableContent">{item.unlock}</td>
+                        </tr>
+                    ));
+        }
+    }
 
     return (
         <div className="entityDesign">
             <h2 className="entityHeader">{type.toUpperCase()}</h2>
+            <p className="entitySubHeader">{entityText}</p>
+            <table className="tableEntity">
+                <thead>
+                    <tr>
+                        {entityHeaderData.map((item) => {
+                            return (
+                               <th key={item}>{item}</th>
+                            )
+                        })}
+                    </tr>
+                </thead>
+                <tbody>
+                    {fulfillTable()}
+                </tbody>
+            </table>
         </div>
     )
 }
